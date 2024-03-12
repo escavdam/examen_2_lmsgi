8ddd6ad6c209b24e91a66526acc48fc9caebce18
Makram <Mkelghadir@gmail.com>
2024-03-12 13:23:44 +0100

    He hecho un fetch

--- fechador.js
+++ fechador.js
@@ -1,1 +1,16 @@
-fetch("https://piticli.glitch.me/")
+console.log("Fechador.js online")
+const cartaDeAnimal = document.querySelector("#animal")
+
+async function animalChooser() {
+    api = `https://piticli.glitch.me/animales/random`
+    const request = await fetch(api)
+    const choosenAnimal = await request.json()
+
+    const { uuid, nombre, fechaNacimiento, proximaVisita, personalidad, vacunas, imagen } = choosenAnimal
+    
+    console.log(choosenAnimal)
+    return choosenAnimal
+}
+
+animalChooser()
+
