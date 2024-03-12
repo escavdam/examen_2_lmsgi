b12b2cd9c32f38a24e33d2bd719fbd4e707877a2
Makram <Mkelghadir@gmail.com>
2024-03-12 14:45:52 +0100

    implementacion total de la carta del animal

--- fechador.js
+++ fechador.js
@@ -8,9 +8,34 @@ async function animalChooser() {
 
     const { uuid, nombre, fechaNacimiento, proximaVisita, personalidad, vacunas, imagen } = choosenAnimal
     
-    console.log(choosenAnimal)
+    
     return choosenAnimal
 }
 
-animalChooser()
+async function qrCodeGenerator() {
+    const qrCode2Generate = await animalChooser()
+    api = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrCode2Generate}`
 
+    return
+}
+
+
+async function crearCartaDeAnimal() {
+    const wantedAnimal = await animalChooser()
+    console.log(wantedAnimal)
+
+
+    cartaDeAnimal.innerHTML =
+    `
+    <img id="imagen" src="${wantedAnimal.imagen}" alt="no imagen">
+    <h1 id="nombre" >${wantedAnimal.nombre}</h1>
+    <p id="fechaNacimiento" >Fecha de nacimiento: ${wantedAnimal.fechaNacimiento}</p>
+    <p id="proximaVisita" >Proxima Vista: ${wantedAnimal.proximaVisita}</p>
+    <li id="personalidad" >Personalidad: ${wantedAnimal.personalidad}</li>
+    <li id="vacunas" >Vacunas: ${wantedAnimal.vacunas}</li>
+    <p id="uuid" >${wantedAnimal.uuid}</p>
+    <img id="qrCode" src="https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${wantedAnimal.uuid}" alt="QR code broken lol">
+    `
+}
+
+crearCartaDeAnimal()
